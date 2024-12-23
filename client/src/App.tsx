3e089207b63c91c238e0bb4500c00c1babe4ebd3
Tanishka <tanishkadeep09@gmail.com>
2024-12-23 13:58:17 +0530

    ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,70 @@
+import { useEffect, useRef, useState } from "react";
+
 function App() {
-  return <div>hi there</div>;
+  const [messages, setMessages] = useState(["hi there", "hello"]);
+  const wsRef = useRef<WebSocket>();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    const ws = new WebSocket("http://localhost:8080");
+    ws.onmessage = (event) => {
+      setMessages((m) => [...m, event.data]);
+    };
+    wsRef.current = ws;
+
+    ws.onopen = () => {
+      ws.send(
+        JSON.stringify({
+          type: "join",
+          payload: {
+            roomId: "red",
+          },
+        })
+      );
+    };
+
+    //cleanup
+    return () => {
+      ws.close();
+    };
+  }, []);
+
+  return (
+    <div className="h-screen bg-black">
+      <br />
+      <br />
+      <br />
+      <div className="h-[75vh]">
+        {messages.map((message) => (
+          <div className="m-8">
+            <span className="bg-white text-black rounded p-4 ">{message}</span>
+          </div>
+        ))}
+      </div>
+      <div className="w-full bg-white flex">
+        <input ref={inputRef} id="message" className="flex-1 p-4"></input>
+        <button
+          onClick={() => {
+            const message = inputRef.current?.value;
+
+            if (message && wsRef.current) {
+              wsRef.current.send(
+                JSON.stringify({
+                  type: "chat",
+                  payload: {
+                    message: message,
+                  },
+                })
+              );
+            }
+          }}
+          className="bg-purple-600 text-white p-4"
+        >
+          Send message
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
